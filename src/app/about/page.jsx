41292426e394b54fe5f46a987ae148c25863a298
vikram <vikram87in@gmail.com>
2025-05-18 15:46:58 +0530

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+// import from css modules
+import React from 'react'
+import styles from './page.module.css'
+
+function About() {
+  return (
+    <h1 className={styles.title}>About Page</h1>
+  )
+}
+
+export default About
